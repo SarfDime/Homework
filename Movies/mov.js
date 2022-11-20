555b8ba1d25af713ea810c84bdc9f10b1ea6decb
SarfDime <sarfdime@yahoo.com>
2022-11-20 20:29:49 +0100

    Update

--- Movies/mov.js
+++ Movies/mov.js
@@ -1,13 +1,23 @@
 var dar = document.getElementById("darkb");
 
+var tex1 = document.getElementById("tex1");
+var tex2 = document.getElementById("tex2");
+var tex3 = document.getElementById("tex3");
+
 check();
 
 function check() {
     if (localStorage.getItem("dark-mode") === "true") {
+        tex1.style.textShadow = "1px 1px 1px #000000";
+        tex2.style.textShadow = "1px 1px 1px #000000";
+        tex3.style.textShadow = "1px 1px 1px #000000";
         darkb.src = "img/sun.png";
         document.body.classList.add("dark-mode");
     } else {
         darkb.src = "img/moon.png";
+        tex1.style.textShadow = "0px 0px #000000";
+        tex1.style.textShadow = "0px 0px #000000";
+        tex1.style.textShadow = "0px 0px #000000";
         document.body.classList.remove("dark-mode");
     }
 }
@@ -15,10 +25,16 @@ function check() {
 dar.onclick = function darP() {
     document.body.classList.toggle("dark-mode");
     if (document.body.classList.contains("dark-mode")) {
+        tex1.style.textShadow = "1px 1px 1px #000000";
+        tex2.style.textShadow = "1px 1px 1px #000000";
+        tex3.style.textShadow = "1px 1px 1px #000000";
         darkb.src = "img/sun.png";
         localStorage.setItem("dark-mode", true);
     } else {
         darkb.src = "img/moon.png";
+        tex1.style.textShadow = "0px 0px #000000";
+        tex2.style.textShadow = "0px 0px #000000";
+        tex3.style.textShadow = "0px 0px #000000";
         localStorage.setItem("dark-mode", false);
     }
 }
